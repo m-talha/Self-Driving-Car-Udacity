18b60be848b88be52de6d2e4048c13dd19c344fb
M Talha <mtalha1990@hotmail.com>
2021-05-27 14:22:34 +0100

    Implemented waypoint updates and basic car control to follow waypoints

--- P9-Capstone/ros/src/twist_controller/twist_controller.py
+++ P9-Capstone/ros/src/twist_controller/twist_controller.py
@@ -1,3 +1,8 @@
+import rospy
+
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -6,9 +11,83 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
-        pass
+        # Yaw controller for steering
+        self.yaw_controller = YawController(kwargs['wheel_base'],
+                                            kwargs['steer_ratio'],
+                                            0.1,
+                                            kwargs['max_lat_accel'],
+                                            kwargs['max_steer_angle'])
 
+        # PID controller for throttle
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn = 0.   # Minimum throttle value
+        mx = 0.2  # Maximum throttle value
+        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
+        # Low pass filter for incoming noisy velocity
+        tau = 0.5  # 1 / (2*pi*tau) = cutoff frequency
+        ts = 0.02  # Sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+
+        self.vehicle_mass = kwargs['vehicle_mass']
+        self.fuel_capacity = kwargs['fuel_capacity']
+        self.brake_deadband = kwargs['brake_deadband']
+        self.decel_limit = kwargs['decel_limit']
+        self.accel_limit = kwargs['accel_limit']
+        self.wheel_radius = kwargs['wheel_radius']
+
+        self.last_time = rospy.get_time()
+
+    # Called by the main controller in dbw_node.py
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        target_linear_vel = kwargs['linear_velocity']
+        angular_vel = kwargs['angular_velocity']
+        current_vel = kwargs['current_vel']
+
+        # Resets the integral value if not in DBW mode
+        if not kwargs['dbw_enabled']:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+
+        # Get the current velocity filtered using Low Pass filter
+        current_vel = self.vel_lpf.filt(current_vel)
+
+        # Get the steering
+        steering = self.yaw_controller.get_steering(
+            target_linear_vel, angular_vel, current_vel)
+
+        # Difference between desired and current velocity
+        vel_error = target_linear_vel - current_vel
+        self.last_vel = current_vel
+
+        # Sample time for PID control step
+        current_time = rospy.get_time()
+        sampling_time = current_time - self.last_time
+        self.last_time = current_time
+
+        # Use PID control to get the next throttle output
+        throttle = self.throttle_controller.step(vel_error, sampling_time)
+
+        # Apply simple control logic for braking
+        brake = 0
+        # If target velocity = 0, let go of throttle and apply braking
+        if target_linear_vel == 0. and current_vel < 0.1:
+            throttle = 0
+            brake = 700  # Torque: Nm - hold the car stationary when stopped
+
+        # If error is negative and not applying throttle, braking required
+        elif throttle < 0.1 and vel_error < 0:
+            throttle = 0
+            # Deceleration required
+            decel = max(vel_error, self.decel_limit)
+            # Corresponding braking required
+            # Use of abs as decel is negative
+            brake = abs(decel) * self.vehicle_mass * \
+                self.wheel_radius  # Torque: Nm
+
+        # return 1., 0., 0.
+        return throttle, brake, steering
--- P9-Capstone/ros/src/waypoint_updater/waypoint_updater.py
+++ P9-Capstone/ros/src/waypoint_updater/waypoint_updater.py
@@ -6,6 +6,7 @@ from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
 
 import math
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -22,7 +23,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200  # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 #200 - car doesn't stay in lane  # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -37,16 +38,73 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher(
             'final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoints_tree = None
 
-        rospy.spin()
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(50)
+
+        while not rospy.is_shutdown():
+            # if members are initialised
+            if self.pose and self.waypoints_2d:
+                # Compute and publish the closest waypoint
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            # Sleep until rate achieved
+            rate.sleep()
+
+    def get_closest_waypoint_idx(self):
+        # For the current car pose
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        # Use the KD-tree to get the closest waypoint index
+        # Only want the single closest waypoint
+        # query() returns tuple as (distance, index)
+        closest_idx = self.waypoints_tree.query([x, y], 1)[1]
+
+        # Check if returned waypoint is in front of car
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx-1]
+
+        # Equation for hyperplane through closest_coords
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        # Get the dot product of vectors:
+        # previous -> closest, closest -> pose
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+
+        # If dp is positive, both vectors point in same direction.
+        # Therfore, waypoint is behind so get the next waypoint
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+
+    def publish_waypoints(self, closest_idx):
+        lane = Lane()
+        # Waypoints to publish are the next LOOKAHEAD_WPS from the current waypoint
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
-        # TODO: Implement
+        # Store the car's pose
+        self.pose = msg
         pass
 
     def waypoints_cb(self, waypoints):
-        pass
+        self.base_waypoints = waypoints
+
+        if not self.waypoints_2d:
+            # Convert each waypoint to 2D coordinates
+            self.waypoints_2d = [[waypoint.pose.pose.position.x,
+                                  waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            # Use KDTree to look up closest point in space efficiently (log n)
+            self.waypoints_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
